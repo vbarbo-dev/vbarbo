59d5196348f96ffd022489be889b73e392a4a148
vbarbo-dev <vinicius@vbarbo.com.br>
2026-05-08 17:40:19 -0300

    Feat: Header navigation

---
+++ src/data/navigation.ts
@@ -0,0 +1,26 @@
+export const navigation = [
+  {
+    key: 'about',
+    href: '#about',
+  },
+  {
+    key: 'experience',
+    href: '#experience',
+  },
+  {
+    key: 'skills',
+    href: '#skills',
+  },
+  {
+    key: 'services',
+    href: '#services',
+  },
+  {
+    key: 'projects',
+    href: '#projects',
+  },
+  {
+    key: 'contact',
+    href: '#contact',
+  },
+];
