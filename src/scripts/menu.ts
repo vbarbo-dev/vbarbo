19f2126e45cacb585ead56def22809937fffb849
vbarbo-dev <vinicius@vbarbo.com.br>
2026-05-29 14:55:19 -0300

    Update: Menu mobile and header UX

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,9 +1,19 @@
 export function headerMenu() {
   const button = document.querySelector('[data-open-menu]');
   const menu = document.querySelector('.menu');
+  const menuItems = menu?.querySelectorAll('.menu-item');
 
   button?.addEventListener('click', () => {
     button?.classList.toggle('-active');
     menu?.classList.toggle('-open');
   });
+
+  if(menuItems) {
+    menuItems.forEach(item => {
+      item.addEventListener('click', () => {
+        menu?.classList.remove('-open');
+        button?.classList.remove('-active');
+      });
+    });
+  }
 }
