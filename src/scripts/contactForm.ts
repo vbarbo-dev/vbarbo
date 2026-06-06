3cda3a7fe80b3504639e3180b1f7fb462145d14f
vbarbo-dev <vinicius@vbarbo.com.br>
2026-06-06 11:44:42 -0300

    Fix: Some adjustments to form and webmanifest

--- src/scripts/contactForm.ts
+++ src/scripts/contactForm.ts
@@ -5,39 +5,38 @@ export function form() {
     const button = form.querySelector('button') as HTMLButtonElement;
 
     form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        button.disabled = true;
+      event.preventDefault();
+      button.disabled = true;
 
-        const formData = new FormData(form);
-        const data = Object.fromEntries(formData.entries());
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData.entries());
 
-        // Honeypot anti-spam
-        if (data.website) { return; }
+      // Honeypot anti-spam
+      if (data.website) { return; }
 
-        try {
-          const response = await fetch('/api/contact',
-            {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(data),
-            }
-          );
+      try {
+        const response = await fetch('/api/contact',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+          }
+        );
 
-          if (!response.ok) { throw new Error('Erro ao enviar') }
+        if (!response.ok) { throw new Error('Erro ao enviar') }
 
-          const result = await response.json();
-          console.log(result);
+        const result = await response.json();
+        console.log(result);
 
-          console.info('Mensagem enviada');
-          form.reset();
-          button.disabled = false;
-        } catch (error) {
-          console.error(error);
-          button.disabled = false;
-        }
+        console.info('Mensagem enviada');
+        form.reset();
+        button.disabled = false;
+      } catch (error) {
+        console.error(error);
+        button.disabled = false;
       }
-    );
+    });
   }
 }
